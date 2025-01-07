f420e99f18636f2f9857a9bfc16eda9983c6fc47
Nathalia <nathaliawitdolatko@gmail.com>
2025-01-07 22:13:40 +0100

    navigation, js og et par styling rettelser

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,10 @@ const nav = document.querySelector(".navbar");
 
 hamburgerMenu.addEventListener("click", () => {
     nav.classList.toggle("active")
+    if (nav.classList.contains("active")) {
+        document.querySelector(".menu-icon").style.color = "#fafafd";
+
+    } else {
+        document.querySelector(".menu-icon").style.color = "#08080d"; 
+    }
 });
